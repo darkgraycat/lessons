692dd2e8f4207c52173e07ef7a6de12ca0ffd5e4
darkgraycat <jburyanov@gmail.com>
2021-04-04 23:42:08 +0300

    lesson 29 done

--- lessons/lesson29/index.js
+++ lessons/lesson29/index.js
@@ -1,3 +1,48 @@
+const printPlaylist = () => {
+	const playList = [
+		{
+			author: "LED ZEPPELIN",
+			song: "STAIRWAY TO HEAVEN"
+		},
+		{
+			author: "QUEEN",
+			song: "BOHEMIAN RHAPSODY"
+		},
+		{
+			author: "LYNYRD SKYNYRD",
+			song: "FREE BIRD"
+		},
+		{
+			author: "DEEP PURPLE",
+			song: "SMOKE ON THE WATER"
+		},
+		{
+			author: "JIMI HENDRIX",
+			song: "ALL ALONG THE WATCHTOWER"
+		},
+		{
+			author: "AC/DC",
+			song: "BACK IN BLACK"
+		},
+		{
+			author: "QUEEN",
+			song: "WE WILL ROCK YOU"
+		},
+		{
+			author: "METALLICA",
+			song: "ENTER SANDMAN"
+		}
+	];
+
+	const listEl = document.createElement('ul')
+	for (item of playList) {
+		let li = document.createElement('li')
+		li.innerHTML = `${item.author} - <b>${item.song}</b>`
+		listEl.appendChild(li)
+	}
+	document.getElementById('songsContainer').appendChild(listEl)
+}
+
 const openModal = () => {
 	document.querySelector('.modal').classList.remove('hide')
 }
